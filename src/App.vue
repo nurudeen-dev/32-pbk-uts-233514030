d9e65ea6b627d67d14c6fc98d98255fb3939110c
nurudeen-dev <ahmadnurudeen20@gmail.com>
2025-05-07 14:51:19 +0700

    input tugas

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,43 @@ const tugas = ref([
   {
     id: 1,
     nama: 'Tugas 1',
-    status: 'Belum Selesai',
+    status:false,
   },
   {
     id: 2,
     nama: 'Tugas 2',
-    status: 'Belum Selesai',
+    status:false,
   },
   {
     id: 3,
     nama: 'Tugas 3',
-    status: 'Belum Selesai',
+    status:false,
   },
 ])
+const newTugas = ref('')
+
+const tambahTugas = () => {
+  if (newTugas.value) {
+    tugas.value.push({
+      id: tugas.value.length + 1,
+      nama: newTugas.value,
+      status: false,
+    })
+    newTugas.value = ''
+  }
+}
+
 </script>
 
 <template>
   <div>
+
+    <input type="text" v-model="newTugas" @keyup.enter="tambahTugas" placeholder="Tambah Tugas">
+    <button @click="tambahTugas">Tambah Tugas</button>
+
     <ul>
       <li v-for="tugas in tugas" :key="tugas.id">
-        {{ tugas.nama }} - {{ tugas.status }}
+        {{ tugas.nama }} - {{ tugas.status ? 'Sudah Selesai' : 'Belum Selesai' }}
       </li>
     </ul>
   </div>
